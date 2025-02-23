2392df97a7703ee953a9576a69561f0d02c024d6
Jordi Petit <jordi.petit@gmail.com>
2025-02-23 10:01:17 +0100

    Add Python tests

--- src/tests/python/test_testing_playground.py
+++ src/tests/python/test_testing_playground.py
@@ -17,3 +17,17 @@ def test_add3i():
     jutge = j.JutgeApiClient()
     s = jutge.testing.playground.add3i(1, 2, 3)
     assert s == 6
+
+
+def test_negate():
+    """Checks that ifiles and ofiles work."""
+
+    jutge = j.JutgeApiClient()
+    download = jutge.misc.get_logo()
+    download.write("logo.png")
+    file = open("logo.png", "rb")
+    download = jutge.testing.playground.negate(file)
+    download.write("negated_logo.png")
+    # PNG files start with an 8-byte signature: 89 50 4E 47 0D 0A 1A 0A
+    signature = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]
+    assert download.data[:8] == bytes(signature), "The logo has not a PNG signature"
