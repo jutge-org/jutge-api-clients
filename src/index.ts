22e468c43613a5f785b8209d59632c0e511b30e7
Jordi Petit <jordi.petit@gmail.com>
2025-02-24 18:52:56 +0100

    Add colors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { genJavaScriptClient } from '@/clients/javascript/generator'
 import { genPhpClient } from '@/clients/php/generator'
 import { genPythonClient } from '@/clients/python/generator'
 import { genTypeScriptClient } from '@/clients/typescript/generator'
+import chalk from 'chalk'
 
 const url = 'https://api.jutge.org/api/dir'
 const destination = 'out'
@@ -11,9 +12,20 @@ const destination = 'out'
 const request = await fetch(url)
 const directory = await request.json()
 
+console.log(chalk.blue('Python'))
 await Bun.write(destination + '/jutge_api_client.py', await genPythonClient(directory))
+
+console.log(chalk.blue('TypeScript'))
 await Bun.write(destination + '/jutge_api_client.ts', await genTypeScriptClient(directory))
-await Bun.write(destination + '/jutge_api_client.js', await genJavaScriptClient(directory))
-await Bun.write(destination + '/jutge_api_client.cpp', await genCppClient(directory))
-await Bun.write(destination + '/jutge_api_client.php', await genPhpClient(directory))
+
+console.log(chalk.blue('Java'))
 await Bun.write(destination + '/JutgeApiClient.java', await genJavaClient(directory))
+
+console.log(chalk.blue('C++'))
+await Bun.write(destination + '/jutge_api_client.cpp', await genCppClient(directory))
+
+console.log(chalk.blue('JavaScript'))
+await Bun.write(destination + '/jutge_api_client.js', await genJavaScriptClient(directory))
+
+console.log(chalk.blue('PHP'))
+await Bun.write(destination + '/jutge_api_client.php', await genPhpClient(directory))
