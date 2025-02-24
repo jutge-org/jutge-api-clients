59cff4073ab9a62a1147112db5cc489907a8425e
Jordi Petit <jordi.petit@gmail.com>
2025-02-24 17:34:44 +0100

    Fake Java unit tests

--- src/tests/java/Test.java
+++ src/tests/java/Test.java
@@ -1,26 +1,53 @@
 import java.io.FileOutputStream;
 
+class TestException extends Exception {
+}
+
 public class Test {
-    public static void main(String[] args) throws Exception {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    static void check(boolean b) throws TestException {
+        if (!b) {
+            throw new TestException();
+        }
+    }
+
+    static void test_tables() throws Exception {
         JutgeApiClient client = new JutgeApiClient();
 
         JutgeApiClient.AllTables tables = client.tables.get();
-        System.out.println(tables.languages.size());
+        check(tables.languages.size() == 5);
+    }
 
-        var languages = client.tables.getLanguages();
-        for (JutgeApiClient.Language l : languages.values()) {
-            System.out.println(l.language_id + " " + l.eng_name);
-        }
+    static void test_logo() throws Exception {
+        JutgeApiClient client = new JutgeApiClient();
 
         JutgeApiClient.Download download = client.misc.getLogo();
-        System.out.println(download.name);
-        System.out.println(download.type);
+        assert download.name == "jutge.png";
+        assert download.type == "image/png";
 
         FileOutputStream fos = new FileOutputStream("logo.png");
         fos.write(download.data);
         fos.close();
+    }
 
-        client.auth.logout();
-        client.meta = null;
+    public static void main(String[] args) throws Exception {
+        try {
+            test_tables();
+            test_logo();
+
+            System.out.println(ANSI_GREEN + "All Java tests passed" + ANSI_RESET);
+        } catch (TestException error) {
+            System.out.println(ANSI_RED + "Java test failed" + ANSI_RESET);
+        }
     }
 }
