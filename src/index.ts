979d303f01ecb558a33c0177d2e38b19a436c878
Pau Fernández <pau.fernandez@upc.edu>
2025-03-14 21:08:44 +0100

    format

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { Command } from "@commander-js/extra-typings"
-import type { Language } from "./gen-client"
-import { clients, generateClient } from "./gen-client"
+import { Command } from '@commander-js/extra-typings'
+import type { Language } from './gen-client'
+import { clients, generateClient } from './gen-client'
 
 const languages = Object.keys(clients)
     .map((c) => `'${c}'`)
-    .join(", ")
+    .join(', ')
 
-const cmd = new Command("jutge-api-client")
-    .description("Generate a client for the Jutge API")
-    .option("-d, --destination <dir>", "The directory where the client will be generated", "client-out")
-    .argument("<language>", `The language of the client to generate (${languages})`)
+const cmd = new Command('jutge-api-client')
+    .description('Generate a client for the Jutge API')
+    .option('-d, --destination <dir>', 'The directory where the client will be generated', 'client-out')
+    .argument('<language>', `The language of the client to generate (${languages})`)
     .action(async (language: string, { destination }) => {
         await generateClient(language as Language, destination)
     })
